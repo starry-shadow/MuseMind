ab579e6c1ff5b38cbf123bf7dfd5d48b1ca839d2
Shikhar Verma <shikharv2@protonmail.com>
2025-09-27 17:41:59 -0400

    the gemini prompting for Urania

--- backend/main.py
+++ backend/main.py
@@ -5,7 +5,10 @@ from google import genai
 # The client gets the API key from the environment variable `GEMINI_API_KEY`.
 client = genai.Client()
 
+# Urania the astronomy and astrology muse
+constellation=input("Enter a constellation:\n")
+# print("constellation is "+constellation)
 response = client.models.generate_content(
-    model="gemini-2.5-flash", contents="Give me the history of the Roman Empire"
+    model="gemini-2.5-flash", contents=f"Give the story behind {constellation}. What lesson does that story teach?"
 )
 print(response.text)
